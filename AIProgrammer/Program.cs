d427d3f434ff9e569fd8342f7f4c1ddc35536569
jurkstas <jurkstas1@yahoo.com>
2020-12-29 20:50:43 +0100

    Update Program.cs

--- AIProgrammer/Program.cs
+++ AIProgrammer/Program.cs
@@ -32,6 +32,7 @@ namespace AIProgrammer
         private static DateTime _startTime = DateTime.Now; // Time the program was started.
         private static string _appendCode = null; // Program code, containing functions, that will be appended to main program code
         private static TargetParams _targetParams = new TargetParams { TargetString = "0123456789" }; // Used for displaying the target fitness
+        private static TargetParams _targetParams = new TargetParams { TargetString = "makerspace48" }; // Used for displaying the target fitness
 
         #endregion
 
@@ -40,6 +41,7 @@ namespace AIProgrammer
         private static double _crossoverRate = 0.8; // Percentage chance that a child genome will use crossover of two parents.
         private static double _mutationRate = 0.1; // Percentage chance that a child genome will mutate a gene.
         private static int _genomeSize = 150; // Number of programming instructions in generated program (size of genome array). loops).
+        private static int _PopulationSize = 100;
         private static int _maxGenomeSize = 1000; // The max length a genome may grow to (only applicable if _expandAmount > 0).
         private static int _maxIterationCount = 5000; // Max iterations a program may run before being killed (prevents infinite loops).
         private static int _expandAmount = 1; // The max genome size will expand by this amount, every _expandRate iterations (may help learning). Set to 0 to disable.
